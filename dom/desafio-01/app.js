a83ed163dfc5f5e43e3061a758bf9b4f5706d504
luucasor <lucas@gatilabs.com.br>
2019-02-12 21:14:49 -0200

    Desafio 01, concluído

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Lucas',
+        idade: 29,
+        imagem: 'http://jornadatecnologia.com/wp-content/uploads/2017/01/cropped-top.png'
+    }
+})
